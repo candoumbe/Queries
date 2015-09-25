df4cd1ebeca19ef0a194919baae825ee5b5c658d
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2015-09-25 15:03:42 +0200

    Moved tests projects to DNX style projects Replaced NUnit with xUnit as unit tests runner

--- Queries.Renderers.Postgres.Tests/PostgresRendererTest.cs
+++ Queries.Renderers.Postgres.Tests/PostgresRendererTest.cs
@@ -1,45 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using System.Collections.Generic;
 using Queries.Core;
 using Queries.Core.Builders;
-using Queries.Core.Extensions;
+using Xunit;
 using static Queries.Core.Builders.Fluent.QueryBuilder;
 
 namespace Queries.Renderers.Postgres.Tests
 {
-    
     public class PostgresRendererTest
     {
-
-
         private class Cases
         {
-            public IEnumerable<TestCaseData> SelectTestCases
+            //public IEnumerable<TestCaseData> SelectTestCases
+            //{
+            //    get
+            //    {
+            //        yield return new TestCaseData(Select("*").From("Table"))
+            //            .SetName($@"{nameof(Select)}(""*"").{nameof(SelectQuery.From)}(""Table"")")
+            //            .Returns(@"SELECT * FROM ""Table""");
+
+            //        yield return new TestCaseData(Select("Employees.*").From("Table"))
+            //            .SetName($@"{nameof(Select)}(""Employees.*"").{nameof(SelectQuery.From)}(""Table"")")
+            //            .Returns(@"SELECT ""Employees"".* FROM ""Table""");
+
+            //        yield return new TestCaseData(Select("*").From("Table".Table("t")))
+            //            .SetName($@"{nameof(Select)}(""*"").{nameof(SelectQuery.From)}(""Table"".{nameof(StringExtensions.Table)}(""t""))")
+            //            .Returns(@"SELECT * FROM ""Table"" ""t""");
+
+
+
+            //    }
+            //}
+
+            public IEnumerable<object[]> SelectTestCases
             {
                 get
                 {
-                    yield return new TestCaseData(Select("*").From("Table"))
-                        .SetName($@"{nameof(Select)}(""*"").{nameof(SelectQuery.From)}(""Table"")")
-                        .Returns(@"SELECT * FROM ""Table""");
-
-                    yield return new TestCaseData(Select("Employees.*").From("Table"))
-                        .SetName($@"{nameof(Select)}(""Employees.*"").{nameof(SelectQuery.From)}(""Table"")")
-                        .Returns(@"SELECT ""Employees"".* FROM ""Table""");
-
-                    yield return new TestCaseData(Select("*").From("Table".Table("t")))
-                        .SetName($@"{nameof(Select)}(""*"").{nameof(SelectQuery.From)}(""Table"".{nameof(StringExtensions.Table)}(""t""))")
-                        .Returns(@"SELECT * FROM ""Table"" ""t""");
-
-
-
+                    yield return new object[]
+                    {
+                        Select("*").From("Table"),
+                        @"SELECT * FROM ""Table"""
+                    };
                 }
             }
         }
 
-
-        [TestCaseSource(typeof(Cases), nameof(Cases.SelectTestCases))]
-        public string SelectTestCases(IQuery query) => query.ForPostgres();
+        [Theory]
+        [MemberData(nameof(Cases.SelectTestCases))]
+        //[TestCaseSource(typeof(Cases), nameof(Cases.SelectTestCases))]
+        public void SelectTestCases(IQuery query, string expectedString) => Assert.Equal(expectedString, query.ForPostgres());
 
 
     }
--- Queries.Renderers.Postgres/PostgresExtensions.cs
+++ Queries.Renderers.Postgres/PostgresExtensions.cs
@@ -1,10 +1,16 @@
-﻿using Queries.Renderers.Postgres;
+﻿using Queries.Core;
+using Queries.Renderers.Postgres;
 
 // ReSharper disable once CheckNamespace
 namespace Queries.Core.Builders
 {
     public static class PostgresExtensions
     {
+        /// <summary>
+        /// Builds the SQL string suitable for <a href="www.postgres.com">Postgres</a> databases
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
         public static string ForPostgres(this IQuery query) => new PostgresqlRenderer().Render(query);
 
         
