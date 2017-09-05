b08808ddfc7ba8221a38a1d887ae763bb1bc9fb4
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2017-09-05 20:11:40 +0200

    Updated README.md Updated .gitignore Added CONTRIBUTING.md

--- src/Queries.Core/Parts/Functions/FunctionAttribute.cs
+++ src/Queries.Core/Parts/Functions/FunctionAttribute.cs
@@ -3,7 +3,7 @@
 namespace Queries.Core.Parts.Functions
 {
     /// <summary>
-    /// Marks a class as a Function class
+    /// Marks a class as a Function
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public sealed class FunctionAttribute : Attribute
