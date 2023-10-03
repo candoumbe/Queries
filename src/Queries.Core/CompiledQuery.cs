2ce3c67c2bcc626c37bfa0d67484da40f444aebb
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-10-03 21:12:03 +0200

    chore(docs) : add documentation for `CompiledQuery`

--- src/Queries.Core/CompiledQuery.cs
+++ src/Queries.Core/CompiledQuery.cs
@@ -7,12 +7,12 @@ using System.Linq;
 namespace Queries.Core;
 
 /// <summary>
-/// The result of calling <see cref="Renderers.QueryRendererBase.Compile(IQuery)"/>.
+/// Represents a query that was compiled alognside its variables
 /// </summary>
 public class CompiledQuery : IEquatable<CompiledQuery>
 {
     /// <summary>
-    /// Gets all <see cref="Variable"/>s of the current
+    /// Gets all <see cref="Variable"/>s of the current <see cref="CompiledQuery"/>.
     /// </summary>
     public IEnumerable<Variable> Variables { get; }
 
@@ -22,10 +22,10 @@ public class CompiledQuery : IEquatable<CompiledQuery>
     public string Statement { get; }
 
     /// <summary>
-    /// Builds a new <see cref="CompiledQuery"/> instance
+    /// Initializes a new instance of the <see cref="CompiledQuery"/> class.
     /// </summary>
-    /// <param name="statement"></param>
-    /// <param name="variables"></param>
+    /// <param name="statement">The statement of the compiled query.</param>
+    /// <param name="variables">The variables of the compiled query.</param>
     public CompiledQuery(string statement, IEnumerable<Variable> variables)
     {
         Statement = statement;
@@ -35,11 +35,18 @@ public class CompiledQuery : IEquatable<CompiledQuery>
     ///<inheritdoc/>
     public override bool Equals(object obj) => Equals(obj as CompiledQuery);
 
-    ///<inheritdoc/>
+    /// <summary>
+    /// Determines whether the specified <see cref="CompiledQuery"/> is equal to the current <see cref="CompiledQuery"/>.
+    /// </summary>
+    /// <param name="other">The <see cref="CompiledQuery"/> to compare with the current <see cref="CompiledQuery"/>.</param>
+    /// <returns>true if the specified <see cref="CompiledQuery"/> is equal to the current <see cref="CompiledQuery"/>; otherwise, false.</returns>
     public bool Equals(CompiledQuery other)
         => other != null && Variables.Intersect(other?.Variables).All(v => Variables.Contains(v)) && Statement == other.Statement;
 
-    ///<inheritdoc/>
+    /// <summary>
+    /// Returns the hash code for the current <see cref="CompiledQuery"/>.
+    /// </summary>
+    /// <returns>A hash code for the current <see cref="CompiledQuery"/>.</returns>
 #if (NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_3)
     public override int GetHashCode() => (Statement, Variables).GetHashCode();
 #else
@@ -47,31 +54,38 @@ public class CompiledQuery : IEquatable<CompiledQuery>
 #endif
 
     /// <summary>
-    /// Checks if <paramref name="left"/> is equal to <paramref name="right"/>.
+    /// Determines whether two specified <see cref="CompiledQuery"/> objects are equal.
     /// </summary>
-    /// <param name="left"></param>
-    /// <param name="right"></param>
-    /// <returns><see langword="true"/> if <paramref name="left"/> is equal to <paramref name="right"/> and <see langword="false"/> otherwise</returns>
+    /// <param name="left">The first <see cref="CompiledQuery"/> to compare.</param>
+    /// <param name="right">The second <see cref="CompiledQuery"/> to compare.</param>
+    /// <returns>true if <paramref name="left"/> is equal to <paramref name="right"/>; otherwise, false.</returns>
     public static bool operator ==(CompiledQuery left, CompiledQuery right)
     {
         return EqualityComparer<CompiledQuery>.Default.Equals(left, right);
     }
 
     /// <summary>
-    /// Checks if <paramref name="left"/> is not equal to <paramref name="right"/>.
+    /// Determines whether two specified <see cref="CompiledQuery"/> objects are not equal.
     /// </summary>
-    /// <param name="left"></param>
-    /// <param name="right"></param>
-    /// <returns><see langword="true"/> if <paramref name="left"/> is not equal to <paramref name="right"/> and <see langword="false"/> otherwise</returns>
+    /// <param name="left">The first <see cref="CompiledQuery"/> to compare.</param>
+    /// <param name="right">The second <see cref="CompiledQuery"/> to compare.</param>
+    /// <returns>true if <paramref name="left"/> is not equal to <paramref name="right"/>; otherwise, false.</returns>
     public static bool operator !=(CompiledQuery left, CompiledQuery right)
     {
         return !(left == right);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns a string that represents the current <see cref="CompiledQuery"/>.
+    /// </summary>
+    /// <returns>A string that represents the current <see cref="CompiledQuery"/>.</returns>
     public override string ToString() => this.Jsonify();
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Deconstructs the current <see cref="CompiledQuery"/> into its variables and statement.
+    /// </summary>
+    /// <param name="variables">When this method returns, contains the variables of the current <see cref="CompiledQuery"/>.</param>
+    /// <param name="statement">When this method returns, contains the statement of the current <see cref="CompiledQuery"/>.</param>
     public void Deconstruct(out IEnumerable<Variable> variables, out string statement)
     {
         variables = Variables;
