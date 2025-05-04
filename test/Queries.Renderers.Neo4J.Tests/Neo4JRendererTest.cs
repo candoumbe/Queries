fd4aa5801423bca33077457ccb7f8ef9f2ea8077
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-05-04 22:37:46 +0200

    refactor(tests): refactor Neo4JRendererTest for improved readability and maintainability
    - Replace IEnumerable<object[]> data sources with TheoryData for test cases
    - Simplify test case structures and initialization
    - Refactor query serialization with updated Json serialization API
    - Remove redundant code and streamline constructor usage

--- test/Queries.Renderers.Neo4J.Tests/Neo4JRendererTest.cs
+++ test/Queries.Renderers.Neo4J.Tests/Neo4JRendererTest.cs
@@ -9,7 +9,8 @@ using Queries.Core.Parts.Columns;
 
 using System;
 using System.Collections.Generic;
-
+using System.Text.Json.Serialization;
+using Queries.Core.Builders.Fluent;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -22,17 +23,81 @@ namespace Queries.Renderers.Neo4J.Tests;
 
 // This project can output the Class library as a NuGet Package.
 // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-public class Neo4JRendererTest
+public class Neo4JRendererTest(ITestOutputHelper output)
 {
-    private readonly ITestOutputHelper _output;
-
-    public Neo4JRendererTest(ITestOutputHelper output) => _output = output;
-
-    public static IEnumerable<object[]> InsertCases
-    {
-        get
+    public TheoryData<BatchQuery, bool> BatchQueriesCases
+     => new()
         {
-            yield return new object[]
+            {
+                new BatchQuery(
+
+                    Select("*").From("Hero").Where("Firstname".Field(), EqualTo, "Bruce"),
+
+                    InsertInto("Disease")
+                        .Values(
+                            "Code".InsertValue("Batman".Literal()),
+                            "Name".InsertValue("Lack of humanity".Literal())
+                        )
+                ),
+                true
+            }
+        };
+
+    public static TheoryData<IBuild<SelectQuery>, Neo4JRendererSettings, string> SelectCases
+        => new()
+        {
+            {
+                Select("*").From("Hero".Table("h")),
+                new Neo4JRendererSettings{ PrettyPrint = false },
+                "MATCH (h:Hero) RETURN h;"
+            },
+            {
+                Select("*")
+                    .From("Hero".Table("h"))
+                    .Where(new WhereClause("Firstname".Field(), EqualTo, "Wayne"))
+                ,
+                new Neo4JRendererSettings{ PrettyPrint = false },
+                "MATCH (h:Hero) WHERE (Firstname = 'Wayne') RETURN h;"
+            },
+            {
+                Select("*").From("Hero".Table("h")),
+                new Neo4JRendererSettings{ PrettyPrint = true },
+                $"MATCH{Environment.NewLine}" +
+                $"    (h:Hero){Environment.NewLine}" +
+                $"RETURN{Environment.NewLine}" +
+                "    h;"
+            },
+            {
+                Select("*").From("Hero"),
+                new Neo4JRendererSettings{ PrettyPrint = false },
+                "MATCH (h:Hero) RETURN h;"
+            },
+            {
+                Select("h1", "h2")
+                    .From("Hero".Table("h1"), "Hero".Table("h2"))
+                    .Where(new CompositeWhereClause {
+                        Logic = ClauseLogic.And,
+                        Clauses =
+                        [
+                            new WhereClause("h1.Lastname".Field(), EqualTo, "Wayne"),
+                            new WhereClause("h2.Lastname".Field(), EqualTo, "Kent")
+                        ]
+                    }),
+                new Neo4JRendererSettings{ PrettyPrint = false },
+                "MATCH (h1:Hero), (h2:Hero) " +
+                "WHERE ((h1.Lastname = 'Wayne') AND (h2.Lastname = 'Kent')) " +
+                "RETURN h1, h2;"
+        }
+    };
+
+    [Theory]
+    [MemberData(nameof(SelectCases))]
+    public void SelectTest(SelectQuery query, Neo4JRendererSettings settings, string expectedString)
+        => IsQueryOk(query, settings, expectedString);
+
+    public static TheoryData<IBuild<InsertIntoQuery>, Neo4JRendererSettings, string> InsertCases
+        => new ()
+        {
             {
                 InsertInto("Hero")
                     .Values(
@@ -41,9 +106,7 @@ public class Neo4JRendererTest
                         "nickname".InsertValue("Batman".Literal())),
                 new Neo4JRendererSettings{ PrettyPrint = false },
                 "CREATE (h:Hero {firstname : 'Bruce', lastname : 'Wayne', nickname : 'Batman'})"
-            };
-
-            yield return new object[]
+            },
             {
                 InsertInto("Hero")
                     .Values(
@@ -53,9 +116,7 @@ public class Neo4JRendererTest
                         "superpowers".InsertValue(null)),
                 new Neo4JRendererSettings{ PrettyPrint = false },
                 "CREATE (h:Hero {firstname : 'Bruce', lastname : 'Wayne', nickname : 'Batman', superpowers : NULL})"
-            };
-
-            yield return new object[]
+            },
             {
                 InsertInto("Hero")
                     .Values(
@@ -64,111 +125,23 @@ public class Neo4JRendererTest
                         "nickname".InsertValue(Upper("Batman".Literal()))),
                 new Neo4JRendererSettings{ PrettyPrint = false },
                 "CREATE (h:Hero {firstname : 'Bruce', lastname : 'Wayne', nickname : UPPER('Batman')})"
-            };
-        }
-    }
-
-    public static IEnumerable<object[]> DeleteCases
-    {
-        get
-        {
-            yield return new object[]
-            {
-                Delete("Heroes").Where("Firstname".Field(), EqualTo, "Wayne"),
-                new Neo4JRendererSettings{ PrettyPrint = false },
-                "MATCH (h:Heroes) WHERE (Firstname = 'Wayne') DELETE h"
-            };
-        }
-    }
-
-    public IEnumerable<object[]> BatchQueriesCases
-    {
-        get
-        {
-            yield return new object[]
-            {
-                new object[] {
-                    new BatchQuery(
-
-                        Select("*").From("Heroe").Where("Firstname".Field(), EqualTo, "Bruce"),
-
-                        InsertInto("Disease")
-                            .Values(
-                                "Code".InsertValue("Batman".Literal()),
-                                "Name".InsertValue("Lack of humanity".Literal())
-                            )
-                    ),
-                    true,
-                }
-            };
-        }
-    }
-    public static IEnumerable<object[]> SelectCases
-    {
-        get
-        {
-            yield return new object[]
-            {
-                Select("*").From("Hero".Table("h")),
-                new Neo4JRendererSettings{ PrettyPrint = false },
-                "MATCH (h:Hero) RETURN h;"
-            };
-
-            yield return new object[]
-            {
-                Select("*")
-                    .From("Hero".Table("h"))
-                    .Where(new WhereClause("Firstname".Field(), EqualTo, "Wayne"))
-                ,
-                new Neo4JRendererSettings{ PrettyPrint = false },
-                "MATCH (h:Hero) WHERE (Firstname = 'Wayne') RETURN h;"
-            };
-
-            yield return new object[]
-            {
-                Select("*").From("Hero".Table("h")),
-                new Neo4JRendererSettings{ PrettyPrint = true },
-                $"MATCH{Environment.NewLine}" +
-                $"    (h:Hero){Environment.NewLine}" +
-                $"RETURN{Environment.NewLine}" +
-                "    h;"
-            };
-
-            yield return new object[]
-            {
-                Select("*").From("Hero"),
-                new Neo4JRendererSettings{ PrettyPrint = false },
-                "MATCH (h:Hero) RETURN h;"
-            };
-
-            yield return new object[]
-            {
-                Select("h1", "h2")
-                    .From("Hero".Table("h1"), "Hero".Table("h2"))
-                    .Where(new CompositeWhereClause {
-                        Logic = ClauseLogic.And,
-                        Clauses = new [] {
-                            new WhereClause("h1.Lastname".Field(), EqualTo, "Wayne"),
-                            new WhereClause("h2.Lastname".Field(), EqualTo, "Kent")
-                        }
-                    }),
-                new Neo4JRendererSettings{ PrettyPrint = false },
-                "MATCH (h1:Hero), (h2:Hero) " +
-                "WHERE ((h1.Lastname = 'Wayne') AND (h2.Lastname = 'Kent')) " +
-                "RETURN h1, h2;"
-            };
-        }
-    }
-
-    [Theory]
-    [MemberData(nameof(SelectCases))]
-    public void SelectTest(SelectQuery query, Neo4JRendererSettings settings, string expectedString)
-        => IsQueryOk(query, settings, expectedString);
+            }
+        };
 
     [Theory]
     [MemberData(nameof(InsertCases))]
     public void InsertTest(InsertIntoQuery query, Neo4JRendererSettings settings, string expectedString)
         => IsQueryOk(query, settings, expectedString);
+
+    public static TheoryData<IBuild<DeleteQuery>, Neo4JRendererSettings, string> DeleteCases
+        => new ()
+        {
+            {
+                Delete("Heroes").Where("Firstname".Field(), EqualTo, "Wayne"),
+                new Neo4JRendererSettings{ PrettyPrint = false },
+                "MATCH (h:Heroes) WHERE (Firstname = 'Wayne') DELETE h"
+            }
+        };
 
     [Theory]
     [MemberData(nameof(DeleteCases))]
@@ -177,8 +150,8 @@ public class Neo4JRendererTest
 
     private void IsQueryOk(IQuery query, Neo4JRendererSettings settings, string expectedString)
     {
-        _output.WriteLine(
-            $"Building : {SerializeObject(query, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented })}{Environment.NewLine}" +
+        output.WriteLine(
+            $"Building : {query.Jsonify(new() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull, WriteIndented = true })}){Environment.NewLine}" +
             $"{nameof(settings)} : {SerializeObject(settings)}");
         query.ForNeo4J(settings).Should().Be(expectedString);
     }
