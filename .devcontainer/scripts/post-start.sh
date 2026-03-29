50ea94a2ae620224215c2fede3e68edf1b731aa7
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2026-03-29 00:37:23 +0000

    feat(devcontainer): add devcontainer configuration and initialization scripts

---
+++ .devcontainer/scripts/post-start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Running postStartCommand..."
+
+echo "Restore dependencies..."
+./build.sh restore
+echo "Dependencies restored successfully."
+
+echo "Runned postStartCommand successfully."
