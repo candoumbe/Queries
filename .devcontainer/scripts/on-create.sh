ce1aab83b7439d259cdb6f9664b3f72dba1af021
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2026-03-29 03:02:00 +0200

    feat(devcontainer): add installation of xdg-utils in on-create script

--- .devcontainer/scripts/on-create.sh
+++ .devcontainer/scripts/on-create.sh
@@ -2,4 +2,9 @@
 set -euo pipefail
 
 echo "Running onCreateCommand..."
+
+# Install xdg-utils for opening the browser from the container
+sudo apt-get update
+sudo apt-get install -y xdg-utils
+
 echo "Runned onCreateCommand successfully."
